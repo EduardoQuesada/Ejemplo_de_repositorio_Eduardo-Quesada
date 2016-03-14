d11465cf2129f7f89ec0a3ed52f1ec8fdba59d3f
Eduardo Quesada <EduardoBigOne@imac-de-eduardo.local>
2016-03-14 17:57:29 -0600

    Initial Commit

---
+++ CuatroBotonesConOutlets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CuatroBotonesConOutlets
+//
+//  Created by Eduardo Quesada on 14/03/16.
+//  Copyright © 2016 Dikom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
